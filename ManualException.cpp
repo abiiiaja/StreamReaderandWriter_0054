8763ef3e9c67b36ea724cc0b0c392ed830abf52a
abiiiaja <kalindraaraya@gmail.com>
2026-06-02 18:59:52 +0700

    kondisi jika throw bertipe data selain integer

--- ManualException.cpp
+++ ManualException.cpp
@@ -14,4 +14,9 @@ int main(){
     {
         cout << "Pengecualian akan di eksekusi" << endl;
     }
+     //kondisi jika throw bertipe data selain integer
+    catch (...)
+    {
+        cout << "default pengecualian diekses kusi" << endl;
+    }
 }
